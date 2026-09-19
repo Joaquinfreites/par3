7eaca242aa93ea0609137495e1a3602b98fce4c4
Freites Joaquin Valentino - 408476 <joautn@gmail.com>
2026-09-19 10:07:56 -0300

    Cambios en la clase hija

--- src/main/java/utnfc/backend/parcial/AlquilerCerrado.java
+++ src/main/java/utnfc/backend/parcial/AlquilerCerrado.java
@@ -4,19 +4,12 @@ public class AlquilerCerrado extends Alquiler {
     private final int kmRecorridos;
 
     public AlquilerCerrado(String id, String cliente, String categoria, String sucursal, int dias, double tarifaDiaria, int kmIncluidos, int kmRecorridos) {
-        this.id = textoObligatorio(id, "id");
-        this.cliente = textoObligatorio(cliente, "cliente");
-        this.categoria = textoObligatorio(categoria, "categoria");
-        this.sucursal = textoObligatorio(sucursal, "sucursal");
-        if (dias <= 0 || tarifaDiaria <= 0 || kmIncluidos < 0 || kmRecorridos < 0) {
-            throw new IllegalArgumentException("dias, tarifa y kilometros deben ser validos");
+        super(id, cliente, categoria, sucursal, dias, tarifaDiaria, kmIncluidos);
+        if (kmRecorridos < 0) {
+            throw new IllegalArgumentException("Los kilometros recorridos deben ser validos");
         }
-        this.dias = dias;
-        this.tarifaDiaria = tarifaDiaria;
-        this.kmIncluidos = kmIncluidos;
         this.kmRecorridos = kmRecorridos;
     }
-
 
     @Override
     public double importe(){
